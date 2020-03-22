f777b9da433aca374f8183d869f0980c242166c6
marcelscruz <marcelcruz@live.com>
2020-03-22 01:03:14 -0300

    Add save wordpair and favourites page

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -8,6 +8,7 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _randomWordPairs = <WordPair>[];
+  final _savedWordPairs = Set<WordPair>();
 
   Widget _buildList() {
     return ListView.builder(
@@ -27,10 +28,59 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _savedWordPairs.contains(pair);
+
     return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: TextStyle(fontSize: 18),
+        title: Text(
+          pair.asPascalCase,
+          style: TextStyle(fontSize: 18),
+        ),
+        trailing: Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+        ),
+        onTap: () {
+          setState(
+            () {
+              if (alreadySaved) {
+                _savedWordPairs.remove(pair);
+              } else {
+                _savedWordPairs.add(pair);
+              }
+            },
+          );
+        });
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _savedWordPairs.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: TextStyle(fontSize: 16),
+                ),
+              );
+            },
+          );
+
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved WordPairs'),
+            ),
+            body: ListView(
+              children: divided,
+            ),
+          );
+        },
       ),
     );
   }
@@ -39,6 +89,12 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('WordPair Generator'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushSaved,
+          )
+        ],
       ),
       body: _buildList(),
     );
